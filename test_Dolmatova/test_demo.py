4c56216767d875204e863e33304e8a0262f691c4
Anna Dolmatova <annadolmatova01@gmail.com>
2022-11-10 13:32:18 -0500

    changed browser to chromium in github actions

--- test_Dolmatova/test_demo.py
+++ test_Dolmatova/test_demo.py
@@ -9,7 +9,6 @@ link = "https://www.saucedemo.com/"
 
 def test_user_is_authorized():
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    time.sleep(10)
     browser.get(link)
     input1 = browser.find_element(By.CSS_SELECTOR, "#user-name")
     input1.send_keys("standard_user")
@@ -17,6 +16,5 @@ def test_user_is_authorized():
     input2.send_keys("secret_sauce")
     button = browser.find_element(By.CSS_SELECTOR, "#login-button")
     button.click()
-    time.sleep(5)
     browser.find_element(By.CSS_SELECTOR, ".shopping_cart_link")
     browser.quit()
