292caa650a3d5834cf6c9a17ddd94ab0deaea466
Anna Dolmatova <annadolmatova01@gmail.com>
2022-11-10 13:05:08 -0500

    fixed black errors test_demo.py

--- test_Dolmatova/test_demo.py
+++ test_Dolmatova/test_demo.py
@@ -8,13 +8,12 @@ link = "https://www.saucedemo.com/"
 
 
 def test_user_is_authorized():
-    browser = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()))
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     time.sleep(10)
     browser.get(link)
-    input1 = browser.find_element(By.CSS_SELECTOR, '#user-name')
+    input1 = browser.find_element(By.CSS_SELECTOR, "#user-name")
     input1.send_keys("standard_user")
-    input2 = browser.find_element(By.CSS_SELECTOR, '#password')
+    input2 = browser.find_element(By.CSS_SELECTOR, "#password")
     input2.send_keys("secret_sauce")
     button = browser.find_element(By.CSS_SELECTOR, "#login-button")
     button.click()
