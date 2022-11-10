cb351ef60164b1d2cca8ac6b375ef814621120f6
KatyaMy <20FamGam20@gmail.com>
2022-11-09 20:57:53 -0500

    test_conflicts1

---
+++ Tests/test_open_cart.py
@@ -0,0 +1,29 @@
+import time
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture(scope="function")
+def driver():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield driver
+
+
+def test_open_cart(driver):
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    time.sleep(1)
+    driver.find_element(By.ID, "shopping_cart_container").click()
+    print("___You IN Product Cart____")
+    assert driver.current_url == "https://www.saucedemo.com/cart.html"
+    driver.find_element(By.ID, "continue-shopping").click()
+    print("______LEFT THE CART_____")
+    assert driver.title == "Swag Labs"
+    time.sleep(2)
+
+    driver.quit()
