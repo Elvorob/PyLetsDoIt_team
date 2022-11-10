51cf6fbc0ffe91629d7dbaf8bfc30b3f07a5d9b8
Anna Dolmatova <annadolmatova01@gmail.com>
2022-11-10 13:34:41 -0500

    fixed flake8 error

--- test_Dolmatova/test_demo.py
+++ test_Dolmatova/test_demo.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-import time
 
 link = "https://www.saucedemo.com/"
 
