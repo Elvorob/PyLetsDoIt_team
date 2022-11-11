c6279f4c926af1f271bada301a56752a4388d577
Anna Dolmatova <annadolmatova01@gmail.com>
2022-11-10 20:04:59 -0500

    added test_add_item_in_the_cart

--- test_Dolmatova/test_demo.py
+++ test_Dolmatova/test_demo.py
@@ -21,3 +21,25 @@ def test_user_is_authorized():
     button.click()
     browser.find_element(By.CSS_SELECTOR, ".shopping_cart_link")
     browser.quit()
+
+
+def test_add_item_in_the_cart():
+    o = webdriver.ChromeOptions()
+    o.headless = True
+    browser = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=o
+    )
+    browser.get(link)
+    input1 = browser.find_element(By.CSS_SELECTOR, "#user-name")
+    input1.send_keys("standard_user")
+    input2 = browser.find_element(By.CSS_SELECTOR, "#password")
+    input2.send_keys("secret_sauce")
+    button = browser.find_element(By.CSS_SELECTOR, "#login-button")
+    button.click()
+    browser.find_element(By.CSS_SELECTOR, ".shopping_cart_link")
+    button_add_to_cart = browser.find_element(
+        By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack"
+    )
+    button_add_to_cart.click()
+    browser.find_element(By.CSS_SELECTOR, ".shopping_cart_badge")
+    browser.find_element(By.CSS_SELECTOR, "#remove-sauce-labs-backpack")
