6727cf396a6384ee8460a0aa1e2e8b2262aaa0b4
Anna Dolmatova <annadolmatova01@gmail.com>
2022-11-23 12:06:58 -0500

    added pytest-html

--- test_Dolmatova/Pages/locators.py
+++ test_Dolmatova/Pages/locators.py
@@ -17,3 +17,8 @@ class ProductsPageLocators:
     ADD_TO_CART_BACKPACK = (By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack")
     CART_BADGE = (By.CSS_SELECTOR, ".shopping_cart_badge")
     REMOVE_BUTTON = (By.CSS_SELECTOR, "#remove-sauce-labs-backpack")
+    SORT_MENU_BUTTON = (By.CSS_SELECTOR, ".product_sort_container")
+    SORT_OPTION_BUTTON_AZ = (By.XPATH, "//option[@value='az']")
+    SORT_OPTION_BUTTON_ZA = (By.XPATH, "//option[@value='za']")
+    SORT_OPTION_BUTTON_LOWHIGH = (By.XPATH, "//option[@value='lohi']")
+    SORT_OPTION_BUTTON_HIGHLOW = (By.XPATH, "//option[@value='hilo']")
--- test_Dolmatova/conftest.py
+++ test_Dolmatova/conftest.py
@@ -22,3 +22,46 @@ def test_fixture():
 
 def pytest_html_report_title(report):
     report.title = "Anna Dolmatova"
+
+
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     pytest_html = item.config.pluginmanager.getplugin("html")
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, "extra", [])
+#     if report.when == "call":
+#         # always add url to report
+#         extra.append(pytest_html.extras.url(browser.current_url))
+#         xfail = hasattr(report, "wasxfail")
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             file_name = report.nodeid.split('::')[-1] + ".png"
+#             file_name_ = "." + directory + file_name
+#             file_name_html = 'assets/' + file_name
+#             browser.get_screenshot_as_file(file_name_)
+#             if file_name_:
+#                 html = f"<div><img src='{file_name_html}' alt='screenshot'"
+#                 html += "onclick='window.open(this.src)' style='width:400px;'"
+#                 html += " align='right'/></div>"
+#                 extra.append(pytest_html.extras.html(html))
+#         report.extra = extra
+
+
+# or
+
+
+#
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     pytest_html = item.config.pluginmanager.getplugin("html")
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, "extra", [])
+#     if report.when == "call":
+#         extra.append(pytest_html.extras.url(driver.current_url))
+#         xfail = hasattr(report, "wasxfail")
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             test_name = os.environ.get('PYTEST_CURRENT_TEST').split(':')[-1].split(' ')[0]
+#             driver.get_screenshot_as_file('screens/screenshot-%s.png' % test_name)
+#             extra.append(pytest_html.extras.image('screens/screenshot-%s.png' % test_name))
+#         report.extra = extra
